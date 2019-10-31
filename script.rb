eecdbe93fcfe9e9cbcae15d211393e225fe04d65
Michael Ion <michael.ion.mi@outlook.com>
2019-10-31 19:28:39 -0400

    part one without the bonus

--- script.rb
+++ script.rb
@@ -2,5 +2,14 @@ class Arena
 end
 
 class Gladiator
+    attr_accessor :name, :weapon
+    def initialize(name, weapon)
+        @name = name
+        @weapon = weapon
+    end
 end
 
+
+max = Gladiator.new("Maximus", "Trident")
+puts(max.name)
+puts(max.weapon)
